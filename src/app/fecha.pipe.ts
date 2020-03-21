6023b22679833b1754666c471d7cfb33a9b4c1a7
Sergio_MS <sergiopsc2@gmail.com>
2020-03-21 16:23:10 -0600

    Fecha con formato

--- src/app/fecha.pipe.ts
+++ src/app/fecha.pipe.ts
@@ -6,13 +6,39 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FechaPipe implements PipeTransform {
 
   transform(value: any, arg): any {
-
+    let mesString: any;
     let final: any;
     let dia = value.getDate().toString();
-    let mes = value.getMonth().toString();
+    let mes = (value.getMonth() + 1).toString();
     let ano = value.getFullYear().toString();
 
-    final= "Nacido el: "+dia+ " de "+mes+ " de "+ ano
+    if(mes==1){
+        mesString="Enero"
+    }else if(mes==2){
+        mesString="Febrero"
+    }else if(mes==3){
+      mesString="Marzo"
+    }else if(mes==4){
+      mesString="Abril"
+    }else if(mes==5){
+      mesString="Mayo"
+    }else if(mes==6){
+      mesString="Junio"
+    }else if(mes==7){
+      mesString="Julio"
+    }else if(mes==8){
+      mesString="Agosto"
+    }else if(mes==9){
+      mesString="Septiembre"
+    }else if(mes==10){
+      mesString="Octubre"
+    }else if(mes==11){
+      mesString="Noviembre"
+      }else if(mes==12){
+        mesString="Diciembre"
+    }
+
+    final= "Nacido el: "+dia+ " de "+mesString+ " de "+ ano
 
     return final;
 
